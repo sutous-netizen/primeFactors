e8bbae32320175a93856045923e9878deaedd301
test <test@test.com>
2026-07-01 09:10:07 +0900

    [feature] 30 page

--- primeFactors/prime.cpp
+++ primeFactors/prime.cpp
@@ -1,4 +1,17 @@
+#include <vector>
+
+using std::vector;
 
 class PrimeFactors {
-
+public:
+	vector<int> of(int i) {
+		vector<int> result = { };
+		if (i == 2) {
+			result.push_back(2);
+		}
+		if (i == 3) {
+			result.push_back(3);
+		}
+		return result;
+	}
 };
--- primeFactors/prime_test.cpp
+++ primeFactors/prime_test.cpp
@@ -1,6 +1,23 @@
 #include "gmock/gmock.h"
 #include "prime.cpp"
 
-TEST(Prime, test1) {
-	EXPECT_EQ(1, 1);
+using namespace testing;
+
+class PrimeFixture : public Test {
+public:
+	PrimeFactors primefacot;
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, of1) {
+	expected = {};
+	EXPECT_EQ(expected, primefacot.of(1));
+}
+TEST_F(PrimeFixture, of2) {
+	expected = {2};
+	EXPECT_EQ(expected, primefacot.of(2));
+}
+TEST_F(PrimeFixture, of3) {
+	expected = { 3 };
+	EXPECT_EQ(expected, primefacot.of(3));
 }
