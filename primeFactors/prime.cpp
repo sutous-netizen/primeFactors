2d3f7eeb9a5e746416a9a06d13f5ab7bff3c2b5f
test <test@test.com>
2026-07-01 09:33:34 +0900

    [refactoring] 41 page

--- primeFactors/prime.cpp
+++ primeFactors/prime.cpp
@@ -7,15 +7,20 @@ public:
 	vector<int> of(int number) {
 		vector<int> result = { };
 		if (number > 1) {
+			int divider = 2;
 			if (number == 4) {
-				while (number % 2 == 0) {
-					result.push_back(2);
-					number /= 2;
+				while (number % divider == 0) {
+					result.push_back(divider);
+					number /= divider;
 				}
 			}
 			else if (number == 6) {
-				result.push_back(2);
-				result.push_back(3);
+				for (divider = 2; number > 1; divider++) {
+					while (number % divider == 0) {
+						result.push_back(divider);
+						number /= divider;
+					}
+				}
 			}
 			else {
 				result.push_back(number);
